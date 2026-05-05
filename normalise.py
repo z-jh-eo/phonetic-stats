37232e024f94306901dd4a1ce41aca1ea8407e61
jiahua <jiahuazh_07@outlook.com>
2026-05-05 21:59:37 +0200

    normalisation added

---
+++ normalise.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+VOWELS = {"a", "e", "ɛ", "i", "o", "ø", "u", "y"}
+
+def is_vowel(phoneme: str) -> bool:
+    return phoneme.lower().strip() in VOWELS
+
+
+def lobanov_norm(df: pd.DataFrame):
+    vowel_df = df[df["phoneme"].apply(is_vowel)]
+
+    stats = vowel_df.groupby("spk")[["F1", "F2"]].agg(["mean", "std"])
+    stats.columns = ["_".join(col) for col in stats.columns]
+
+    df = df.copy()
+    df["F1_normed"] = (df["F1"] - df["spk"].map(stats["F1_mean"])) / df["spk"].map(stats["F1_std"])
+    df["F2_normed"] = (df["F2"] - df["spk"].map(stats["F2_mean"])) / df["spk"].map(stats["F2_std"])
+    return df
+
